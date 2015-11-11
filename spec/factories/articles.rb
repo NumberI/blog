873f509c1f8e63c93af03a382174bc3a8e611738
NumberI <x0987654321@ya.ru>
2015-11-11 18:48:07 +0300

    add last_comment to article

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,12 @@ FactoryGirl.define do
   factory :article do
     title "dfsfsg"
     text "textsss"
+    
+    factory :article_with_comments do
+      after :create do |article, evaluator|
+        create_list :comment, 3, article: article
+      end
+    end
+    
   end
 end
