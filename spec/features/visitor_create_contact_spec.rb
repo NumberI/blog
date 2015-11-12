6ec760a08eaebb172d5424022edac977cdaeda57
NumberI <x0987654321@ya.ru>
2015-11-12 16:40:19 +0300

    update tests (allows a guest to create contact)

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,9 +1,18 @@
-require "spec_helper"
+﻿require "spec_helper"
 
 feature "Contact creation" do
 	scenario "allows access to contact page" do
 		visit "/contacts"
 
-    expect(page).to have_content "Your"
+    expect(page).to have_content I18n.t('contacts.contact_us')
 	end
+  
+  scenario "allows a guest to create contact" do
+    visit "/contacts"
+    fill_in :contact_email, :with => "123@123.com"
+    fill_in :contact_message, :with => "asdasd"
+    click_button "Send message"
+    
+    expect(page).to have_content 'Спасибо!'
+  end
 end
